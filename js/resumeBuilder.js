15a732b7bc45a2318f6b1497ebbb92f27aa465f0
Roger Woodroofe <rogyw@yahoo.co.nz>
2015-09-12 16:20:44 +1200

    add js basics lesson 1 initial test

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,14 @@
 /*
-This is empty on purpose! Your code to build the resume will go here.
- */
+*
+* Fie For Roger's Resume content
+*
+*/
+var awesomeThoughts = "I am Roger and I am AWESOME!";
+
+$("#main").append("Roger Woodroofe");
+
+funThoughts = awesomeThoughts.replace("AWESOME","FUN");
+
+console.log(awesomeThoughts);
+$("#main").append(funThoughts);
+
