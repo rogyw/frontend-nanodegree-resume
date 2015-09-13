0c675fee2d0aa083a7407917df577da3afad69ed
Roger Woodroofe <rogyw@yahoo.co.nz>
2015-09-13 21:20:27 +1200

    feat: add work object and associated values

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -78,7 +78,7 @@ var education = {
   },
   {
     "title": "Workplace First Aid in Outdoors",
-    "name": "NZ Mountain Saftey",
+    "school": "NZ Mountain Saftey",
     "city": "Auckland, NZ",
     "dates": " Sept 2013",
     "url": "http://www.mountainsafety.org.nz/training/Outdoor-First-Aid/Overview.asp"
@@ -91,4 +91,51 @@ var education = {
     "url": "http://www.openpolytechnic.ac.nz/"
   }
   ]
+};
+
+var work = {
+  "jobs": [
+    {
+      "employer": "Contract - Orienteering NZ",
+      "title": "Online Coordinator",
+      "location": "Remote",
+      "dates": "April 2014 - current",
+      "description": "The purpose of the Online Co-ordinator is to ensure the Orienteering NZ is managing its online profile in a best-practice way. Wordpress site administration"
+    },
+    {
+      "employer": "Self Employed (Casual)",
+      "title": "IT Maintenance and Support",
+      "location": "Auckland, NZ",
+      "dates": "2001 - present",
+      "description": "IT support services for several Small Businesses. Computer and network setup, maintenance and support. Application Support. Staff training and procedures documentation. On-call onsite and remote support."
+    },
+    {
+      "employer": "Codenza Limited",
+      "title": "Web Developer and Support",
+      "location": "East Tamaki, Auckland, NZ",
+      "dates": "Nov 2011 - Dec 2012",
+      "description": "Website development and support. CSS, HTML, PHP, Joomla website upgrades, configuration, and new sites. Joomla template development and client support. User documentation. Shopping cart site upgrades. Content updates. Site maintenance. Testing and issue resolution."
+    },
+    {
+      "employer": "Volunteer / Self Employed (Casual)",
+      "title": "Web Developer, Administrator and Support",
+      "location": "Auckland, NZ",
+      "dates": "2001 - 2014",
+      "description": "Provided customised website development adhering to web standards validation. CMS: Silverstripe, Concrete5, Joomla, Wordpress. HTML4+5/XHTML and CSS. MySQL database maintenance. Maintenance and improvement of sites in PHP, Coldfusion5, Javascript, jQuery. Online forms, and database structures; Website support and advice. Management of hosting, domain names, email services; assisted clients and staff with content creation; maintained and implemented smooth change-over; Speed enhancement, upgrades, logs analysis, SEO and troubleshooting."
+    },
+    {
+      "employer": "Symantec New Zealand Limited",
+      "title": "Information Developer",
+      "location": "Auckland, NZ",
+      "dates": "June 1998 - May 1999",
+      "description": "User documentation update and maintenance to adhere to style guidelines. Worked with overseas senior editor.  Provided updates to international translation team. Identified and reported documentation requirements for version releases."
+    },
+    {
+      "employer": "Self Employed (Contract)",
+      "title": "Software Developer",
+      "location": "Auckland, NZ",
+      "dates": "Dec 1996 - June 1998",
+      "description": "Development in C programming language with network protocols NetBIOS, IPX and TCP/IP and Winsock2/WATTTCP/Network Driver specifications. Development team member. Minimal assembly language work. Windows online user help file creation. User manual supplements. Documentation maintenance and project management."
+    }
+    ]
 };
